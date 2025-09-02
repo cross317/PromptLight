704bc90992c4fb2f5d8c83255a6c200fa39762ec
cross317 <mario.linked.mail@gmail.com>
2025-09-02 22:30:38 +0200

    initialcommit

---
+++ script.js
@@ -0,0 +1,22 @@
+const SESSION_KEY = 'pf_session_started';
+
+const hasSessionStarted = sessionStorage.getItem(SESSION_KEY) === '1';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    sessionStorage.setItem(SESSION_KEY, '1');
+
+    const intro = document.getElementById('intro');
+    if (!intro) return;
+
+    if (hasSessionStarted) {
+        intro.remove();
+        return;
+    }
+
+    intro.addEventListener('animationend', (e) => {
+    if (e.animationName === 'fadeOut') {
+        intro.remove();
+    }
+  });
+});
